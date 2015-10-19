88db2900a186fedd359c782bf20dcc7861aedcfe
jedgar747 <jedgar747@googlemail.com>
2015-10-19 13:58:47 +0100

    Age example

--- index.php
+++ index.php
@@ -5,7 +5,19 @@
 <body>
 <p>
     <?php
-    echo "Hello World php";
+    $myAge =18;
+    $personName = "Josh";
+
+    if($myAge >16){
+        print "You can buy specs";
+    }
+    elseif ($myAge >18){
+        print "You can buy mugs and specs";
+    }
+    elseif ($myAge >21){
+        print "You can buy specs, mugs and sausage rolls";
+    }
+    //echo "Hello World php";
     ?>
 </p>
 </body>
