ed1e235e97c1b10bcf8c53e898308f748a0f5fd3
jedgar747 <jedgar747@googlemail.com>
2015-11-02 13:33:44 +0000

    basic login

--- index.php
+++ index.php
@@ -3,12 +3,43 @@
 <head>
 </head>
 <body>
-<p>
+<div class="register-form">
+    2
     <?php
-    echo "hello";
-    ?>
+
+    if(isset($msg) & !empty($msg)){
+
+        echo $msg;
+
+    }
+
+ ?>
+
+    <h1>Login</h1>
+
+    <form action="" method="POST">
+
+        <p><label>User Name : </label>
+
+            <input id="username" type="text" name="username" placeholder="username" /></p>
 
 
-</p>
+
+        <p><label>Password&nbsp;&nbsp; : </label>
+            13
+            <input id="password" type="password" name="password" placeholder="password" /></p>
+
+
+
+        <a class="btn" href="register.php">Signup</a>
+
+        <input class="btn register" type="submit" name="submit" value="Login" />
+
+    </form>
+
+</div>
+
+<?php ?>
+
 </body>
 </html>
