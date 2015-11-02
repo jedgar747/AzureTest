047cc2f1814af855c0a417f09e1d80318924d367
jedgar747 <jedgar747@googlemail.com>
2015-11-02 13:31:21 +0000

    echo hello

--- index.php
+++ index.php
@@ -5,46 +5,7 @@
 <body>
 <p>
     <?php
-    $myAge =15;
-    $personName = "Josh";
-
-    if(($myAge >16) and ($myAge <=18)){
-        print "You can buy specs";
-    }
-    elseif (($myAge >18) and ($myAge <=21)){
-        print "You can buy mugs and specs";
-    }
-    elseif ($myAge >21){
-        print "You can buy specs, mugs and sausage rolls";
-    }
-    else{
-        print "Sorry, you can't buy anything";
-    }
-    //echo "Hello World php";
-    $wantedGood = "mugs";
-
-    switch ($wantedGood) {
-        case "mugs":
-            echo "You have to be 18 to buy mugs";
-            break;
-        case "sausageRoll":
-            echo "You have to be 21 to buy sausage rolls";
-            break;
-        case "specs":
-            echo "You have to be 16 to buy specs";
-            break;
-        default:
-            echo "Nothing set";
-    }
-
-    $provisionedActivities = array("Specs", "Drugs", "Rock", "Roll", "Mugs");
-    foreach($provisionedActivities as $x){
-        if($x == "Mugs"){
-            $provisionedActivities[$x]['value']="hugs";
-        }
-        print "<p>$x</p>";
-    }
-
+    echo "hello";
     ?>
 
 
